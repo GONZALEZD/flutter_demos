4e45c3e01dcccb49a6d217bea1a37243a3fb0498
David GONZALEZ <david.gonzalez.freelance@gmail.com>
2021-01-08 12:06:48 +0100

    Flip Animation demo

---
+++ flip_animation/android/app/src/main/kotlin/com/example/flip_animation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flip_animation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flip_animation/lib/main.dart
@@ -0,0 +1,167 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flip Animation Example'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  bool _displayFront;
+  bool _flipXAxis;
+
+  @override
+  void initState() {
+    super.initState();
+    _displayFront = true;
+    _flipXAxis = true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(this.widget.title),
+        centerTitle: true,
+        actions: [
+          IconButton(icon: RotatedBox(child: Icon(Icons.flip), quarterTurns: _flipXAxis ? 0 : 1,),
+            onPressed: _changeRotationAxis,),
+        ],
+      ),
+      body: DefaultTextStyle(
+        style: TextStyle(color: Colors.white),
+        textAlign: TextAlign.center,
+        child: Center(
+          child: Container(
+            constraints: BoxConstraints.tight(Size.square(200.0)),
+            child: _buildFlipAnimation(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _changeRotationAxis() {
+    setState(() {
+      _flipXAxis = !_flipXAxis;
+    });
+  }
+
+  void _switchCard() {
+    setState(() {
+      _displayFront = !_displayFront;
+    });
+  }
+
+  Widget _buildFlipAnimation() {
+    return GestureDetector(
+      onTap: _switchCard,
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 1200),
+        transitionBuilder: __transitionBuilder,
+        layoutBuilder: (widget, list) => Stack(children: [widget, ...list]),
+        child: _displayFront ? _buildFront() : _buildRear(),
+        switchInCurve: Curves.easeInBack,
+        switchOutCurve: Curves.easeInBack.flipped,
+      ),
+    );
+  }
+
+  Widget __transitionBuilder(Widget widget, Animation<double> animation) {
+    final rotateAnim = Tween(begin: pi, end: 0.0).animate(animation);
+    return AnimatedBuilder(
+      animation: rotateAnim,
+      child: widget,
+      builder: (context, widget) {
+        final isUnder = (ValueKey(_displayFront) != widget.key);
+        var tilt = ((animation.value - 0.5).abs() - 0.5) * 0.003;
+        tilt *= isUnder ? -1.0 : 1.0;
+        final value = isUnder ? min(rotateAnim.value, pi / 2) : rotateAnim.value;
+        return Transform(
+          transform: _flipXAxis ? (Matrix4.rotationY(value)
+            ..setEntry(3, 0, tilt)) : (Matrix4.rotationX(value)
+            ..setEntry(3, 1, tilt)),
+          child: widget,
+          alignment: Alignment.center,
+        );
+      },
+    );
+  }
+
+  Widget _buildFront() {
+    return __buildLayout(
+      key: ValueKey(true),
+      backgroundColor: Colors.blue,
+      faceName: "Front",
+      child: Padding(
+        padding: EdgeInsets.all(32.0),
+        child: ColorFiltered(
+          colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcATop),
+          child: FlutterLogo(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRear() {
+    return __buildLayout(
+      key: ValueKey(false),
+      backgroundColor: Colors.blue.shade700,
+      faceName: "Rear",
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: ColorFiltered(
+          colorFilter: ColorFilter.mode(Colors.white, BlendMode.srcATop),
+          child: Center(child: Text("Flutter", style: TextStyle(fontSize: 50.0))),
+        ),
+      ),
+    );
+  }
+
+
+  Widget __buildLayout({Key key, Widget child, String faceName, Color backgroundColor}) {
+    return Container(
+      key: key,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          child,
+          Positioned(
+            bottom: 8.0,
+            right: 8.0,
+            child: Text(faceName),
+          ),
+        ],
+      ),
+    );
+  }
+}
